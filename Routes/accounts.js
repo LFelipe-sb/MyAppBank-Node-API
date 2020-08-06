0d4c9441f2f0918806680a6f3ecd70e199e52574
Luis Felipe <luisfelipe.sb@outlook.com>
2020-08-06 00:06:30 -0300

    Tratamento de erros, criação e configuração de logs da nos endpoints.

--- Routes/accounts.js
+++ Routes/accounts.js
@@ -3,7 +3,7 @@ import { promises as fs} from 'fs';
 
 const router = express.Router();
 
-router.post('/', async(req, res) => {
+router.post('/', async(req, res, next) => {
     try {
         let account = req.body;
         const data = JSON.parse( await fs.readFile('accounts.json'));
@@ -12,58 +12,66 @@ router.post('/', async(req, res) => {
         data.account.push(account);
         
         await fs.writeFile('accounts.json', JSON.stringify(data, null, 2));
-        
         res.send('Criado com sucesso. ' + JSON.stringify(account));
+
+        logger.info(`POST /Account - ${JSON.stringify(account)}`);
     } catch (err) {
-        res.status(500).send('Ocorreu um erro. ' + err);
+        next(err);
     }
 });
 
-router.get('/', async(_req, res) => {
+router.get('/', async(_req, res, next) => {
     try{
         const data = JSON.parse(await fs.readFile('accounts.json'));
         delete data.nextID;
         res.send(data);
+
+        logger.info(`GET /Account`);
+
     } catch (err) {
-        res.status(500).send('Ocorreu um erro. ' + err);
+        next(err);
     }
 });
 
-router.get('/:id', async(req, res) => {
+router.get('/:id', async(req, res, next) => {
     try{
         const data = JSON.parse(await fs.readFile('accounts.json'));
         const account = data.account.find((account) => account.id === parseInt(req.params.id));
         res.send(account);
+
+        logger.info(`GET /Account/${req.params.id}`);
     } catch(err) {
-        res.status(500).send('Ocorreu um erro. ' + err);
+        next(err);
     }
 });
 
-router.delete('/:id', async(req, res) => {
+router.delete('/:id', async(req, res, next) => {
     try{
         const data = JSON.parse(await fs.readFile('accounts.json'));
         data.account = data.account.filter((account) => account.id !== parseInt(req.params.id));
         await fs.writeFile('accounts.json', JSON.stringify(data, null, 2));
-        res.send('Item excluido com sucesso.')
+        res.send('Item excluido com sucesso.');
+        logger.info(`DELETE /Account/${req.params.id}`);
     } catch(err) {
-        res.status(500).send('Ocorreu um erro. ' + err);
+        next(err);
     }
 });
 
-router.put('/', async(req, res) => {
+router.put('/', async(req, res, next) => {
     try {
         const data = JSON.parse(await fs.readFile('accounts.json'));
         const accountData = req.body;
         const index = data.account.findIndex((item) => item.id === parseInt(accountData.id));
         data.account[index] = accountData;
-        await fs.writeFile('accounts.json', JSON.stringify(data));
+        await fs.writeFile('accounts.json', JSON.stringify(data, 2, null));
         res.send(data);
+        logger.info(`PUT /Account - ${JSON.stringify(account)}`);
     } catch (err) {
-        res.status(500).send('Ocorreu um erro. ' + err);
+        next(err);
     }
 });
 
-router.patch('/updateBalance', async(req, res) => {
+router.patch('/updateBalance', async(req, res, next) => {
     try{
         const data = JSON.parse(await fs.readFile('accounts.json'));
         const accountData = req.body;
@@ -71,11 +79,19 @@ router.patch('/updateBalance', async(req, res) => {
         const index = data.account.findIndex((item) => item.id === parseInt(accountData.id));
         data.account[index].balance = accountData.balance;
 
-        await fs.writeFile('accounts.json', JSON.stringify(data));
+        await fs.writeFile('accounts.json', JSON.stringify(data, null, 2));
         res.send(data.account[index]);
+        logger.info(`PATCH /Account - ${JSON.stringify(account)}`);
     } catch(err) {
-        res.status(500).send('Ocorreu um erro. ' + err);
+        next(err);
     }
+});
+
+//Tratamento de erros.
+router.use((err, req, res, _next) => {
+    global.logger.error(`${req.method} ${req.baseUrl} -> ${err.message}`)
+    res.status(500).send('Ocorreu um erro. ' + err);
+
 });
 
 export default router;
--- index.js
+++ index.js
@@ -2,26 +2,44 @@ import express from 'express';
 import { promises as fs} from 'fs';
 import accountRouter from './Routes/accounts.js'
 
+import winston from 'winston';
+
 const app = express();
 app.use(express.json());
 
 app.use('/account', accountRouter);
 
+const {combine, printf, timestamp, label} = winston.format;
+const myformat = printf(({level, message, label, timestamp}) => {
+    return `${timestamp} [${label}] -> ${level}: ${message}`;
+});
+
+global.logger = winston.createLogger({
+    level: "silly",
+    transports: [
+        new (winston.transports.Console)(),
+        new (winston.transports.File)({filename: "my-bank-api.log"})
+    ],
+    format: combine(
+        label({label: "my-bank-api"}),
+        timestamp(),
+        myformat
+    )
+});
+
 app.listen(3001, async() => {
     try{
         await fs.readFile('accounts.json');
-        console.log('API Started');
+        global.logger.info('API Started');
     } catch(err) {
         const initialJson = {
             nextID: 1,
             account: []
         }
         fs.writeFile('accounts.json', JSON.stringify(initialJson, null, 2)).then(() => {
-            console.log('API Started and created file.');
+            global.logger.info('API Started and created file.');
         }).catch(err => {
-            console.log(err);
+            global.logger.error(err);
         });
     }
 });
-
-
